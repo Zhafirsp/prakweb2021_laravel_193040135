6430ffa1cae06b0705eb17d4a39bab504af6d532
Zhafir <dsquaredea@gmail.com>
2021-10-08 12:51:51 +0700

    Menyelesaikan video belajar laravel episode 4 tentang Blade Templating Engine

--- coba-laravel/resources/views/home.blade.php
+++ coba-laravel/resources/views/home.blade.php
@@ -4,22 +4,13 @@ Muhammad Zhafir Sunandy Pramana
 193040135
 https://github.com/Zhafirsp/prakweb2021_laravel_193040135.git
 Pertemuan 5 - 8 October 2021
-Mempelajari tentang Struktur folder, routes & view
+Mempelajari tentang Struktur folder, routes, view pada episode 3 dan
+Mempelajari tentang Blade Templating Engine pada episode 4 Belajar Laravel *
 */
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/style.css">
-  <title>WPU Blog | Home</title>
-</head>
-<body>
-  <h1>Halaman Home</h1>
+@extends('layouts.main')
 
-  <script src="js/script.js"></script>
-</body>
-</html>
+@section('container')
+  <h1>Halaman Home</h1>
+@endsection 
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,17 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('Home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Muhammad Zhafir",
         "email" => "193040135.muhammad@mail.unpas.ac.id",
         "image" => "Profile.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Rembulan Dibawah Bintang",
+            "slug" => "Rembulan-Dibawah-Bintang",
+            "author" => "Mariana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae alias dignissimos perferendis voluptas adipisci explicabo iure architecto minima est quibusdam veniam, atque fugit illum, quia aliquam ex ipsam, harum a cupiditate libero. Architecto voluptas molestias at explicabo. Nihil doloremque quae cumque eos doloribus, provident suscipit repellat dolores harum autem numquam aliquid vel quibusdam magni cum placeat laboriosam ex laudantium optio neque iusto! Quod doloremque omnis a dicta assumenda recusandae aperiam vero, architecto dolor quos iste maiores quaerat! Impedit illo corporis nihil dignissimos commodi, aliquam incidunt rerum harum a eum, culpa, neque ducimus nostrum at repellat perspiciatis quasi sint fugiat doloribus."
+        ],
+        [
+            "title" => "Dan Saat Kita Bersama",
+            "slug" => "Dan-Saat-Kita-Bersama",
+            "author" => "Babilyone",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae alias dignissimos perferendis voluptas adipisci explicabo iure architecto minima est quibusdam veniam, atque fugit illum, quia aliquam ex ipsam, harum a cupiditate libero. Architecto voluptas molestias at explicabo. Nihil doloremque quae cumque eos doloribus, provident suscipit repellat dolores harum autem numquam aliquid vel quibusdam magni cum placeat laboriosam ex laudantium optio neque iusto! Quod doloremque omnis a dicta assumenda recusandae aperiam vero, architecto dolor quos iste maiores quaerat! Impedit illo corporis nihil dignissimos commodi, aliquam incidunt rerum harum a eum, culpa, neque ducimus nostrum at repellat perspiciatis quasi sint fugiat doloribus."
+        ],
+    ];
+
+
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Rembulan Dibawah Bintang",
+            "slug" => "Rembulan-Dibawah-Bintang",
+            "author" => "Mariana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae alias dignissimos perferendis voluptas adipisci explicabo iure architecto minima est quibusdam veniam, atque fugit illum, quia aliquam ex ipsam, harum a cupiditate libero. Architecto voluptas molestias at explicabo. Nihil doloremque quae cumque eos doloribus, provident suscipit repellat dolores harum autem numquam aliquid vel quibusdam magni cum placeat laboriosam ex laudantium optio neque iusto! Quod doloremque omnis a dicta assumenda recusandae aperiam vero, architecto dolor quos iste maiores quaerat! Impedit illo corporis nihil dignissimos commodi, aliquam incidunt rerum harum a eum, culpa, neque ducimus nostrum at repellat perspiciatis quasi sint fugiat doloribus."
+        ],
+        [
+            "title" => "Dan Saat Kita Bersama",
+            "slug" => "Dan-Saat-Kita-Bersama",
+            "author" => "Babilyone",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae alias dignissimos perferendis voluptas adipisci explicabo iure architecto minima est quibusdam veniam, atque fugit illum, quia aliquam ex ipsam, harum a cupiditate libero. Architecto voluptas molestias at explicabo. Nihil doloremque quae cumque eos doloribus, provident suscipit repellat dolores harum autem numquam aliquid vel quibusdam magni cum placeat laboriosam ex laudantium optio neque iusto! Quod doloremque omnis a dicta assumenda recusandae aperiam vero, architecto dolor quos iste maiores quaerat! Impedit illo corporis nihil dignissimos commodi, aliquam incidunt rerum harum a eum, culpa, neque ducimus nostrum at repellat perspiciatis quasi sint fugiat doloribus."
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
