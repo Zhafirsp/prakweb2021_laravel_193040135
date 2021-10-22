58d74a68e22517cac475bab141ff72135d2fc77f
Zhafir <dsquaredea@gmail.com>
2021-10-22 09:15:15 +0700

    Menyelesaikan video belajar laravel 8 episode 9 tentang Database Seeder dengan menghapus DB sebelumnya yang telah dibuat

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,8 +5,8 @@
 <br>
   @foreach ($posts as $post)
       <article class="mb-5">
-      <h2><a href="/posts/{{ $post->slug }}">{{ $post->title}}</a></h2>
-      <h5>By: {{ $post->author}}</h5>
+      <h2><a href="/posts/{{ $post->slug }}"class="text-decoration-none">{{ $post->title}}</a></h2>
+      <h5 >By: <a href="#" class="text-decoration-none">{{ $post->user->name}}</a></h5>
       <p>{{ $post->excerpt}}</p>
     </article>
   @endforeach
