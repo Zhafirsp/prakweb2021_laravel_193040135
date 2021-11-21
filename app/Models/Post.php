e4a8e921aa94980049205a189972c0eba33a6dd0
Zhafir <dsquaredea@gmail.com>
2021-11-21 14:14:27 +0700

    Menyelesaikan video belajar laravel 8 episode 18 tentang Create Post Form dengan Eloquent Sluggable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     // fillable dan guarded sama saja
     //$fillable = ['title', 'author', 'excerpt', 'body'];
@@ -62,5 +64,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
